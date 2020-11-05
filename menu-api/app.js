b84da7bce41a416fb4f549913e6711ff4f0767e6
Peter Nagy <peter.nagy@adevinta.com>
2020-11-05 22:16:49 +0100

    Add cors

--- menu-api/app.js
+++ menu-api/app.js
@@ -8,12 +8,15 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var menusRouter = require("./routes/menus");
 
+var cors = require("cors");
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
