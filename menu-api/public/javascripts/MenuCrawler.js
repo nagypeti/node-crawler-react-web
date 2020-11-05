495100847a86b673861371138e1602e5dbd1c414
Peter Nagy <peter.nagy@adevinta.com>
2020-11-05 19:52:52 +0100

    Add axios package and add async function fetchData

--- menu-api/public/javascripts/MenuCrawler.js
+++ menu-api/public/javascripts/MenuCrawler.js
@@ -1,13 +1,27 @@
+const axios = require('axios');
 const Menu = require('./Menu');
 
 class MenuCrawler {
     constructor() {
-        this.menus = [];
+        this.menus = []
     }
 
-    getMenu() {
-        return new Menu;
+    async getMenu() {
+        await this.fetchData()
+        return new Menu
     }
+
+    async fetchData() {
+        const url = 'https://millcantin.hu/termekkategoria/etlap/02-menuk';
+        console.log('Fetching data from: ', url)
+        const response = await axios(url).catch(err => console.log(err))
+
+        if (response.status !== 200) {
+            console.log('Error occurred while fetching data')
+            return 'Error occurred while fetching data'
+        }
+        return response
+    };
 }
 
 module.exports = MenuCrawler;
