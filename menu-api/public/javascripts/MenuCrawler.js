a0a1b8f3a25f5e3ab06b229c766cd21ccc7d2f76
Peter Nagy <peter.nagy@adevinta.com>
2020-11-05 21:55:21 +0100

    Extract all the required information from the site
    - price, soup, maindish

--- menu-api/public/javascripts/MenuCrawler.js
+++ menu-api/public/javascripts/MenuCrawler.js
@@ -16,8 +16,8 @@ class MenuCrawler {
         await this.getMenus()
 
         return this.menus.find(menu => {
-            if (menu.title == titleLower) {
-                console.log('Requested menu is: ', menu)
+            if (menu.title === titleLower) {
+                console.log('Requested menu is:', menu)
                 return menu
             }
         })
@@ -34,7 +34,7 @@ class MenuCrawler {
     }
 
     async fetchData(url) {
-        console.log('Fetching data from: ', url)
+        console.log('Fetching data from:', url)
         const response = await axios(url).catch(err => console.log(err))
 
         if (response.status !== 200) {
@@ -51,7 +51,7 @@ class MenuCrawler {
         section.each(function () {
             menuUrls.push(($(this).attr('href')))
         })
-        console.log('Fetching menus from: ', menuUrls)
+        console.log('Fetching menus from:', menuUrls)
         return menuUrls
     }
 
@@ -61,6 +61,16 @@ class MenuCrawler {
         const menu = new Menu()
         section.each(function () {
             menu.title = $(this).find('h1.product_title.entry-title').text().charAt(0).toLowerCase()
+            menu.price = $(this).find('span.woocommerce-Price-amount.amount').first().text()
+
+            const dishes = $(this).find('div.woocommerce-product-details__short-description')
+            menu.soup = dishes.children('p:nth-child(2)').text().slice(6).replace('_', ' - ')
+            if (menu.title == 'a') {
+                menu.mainDish = dishes.children('p:nth-child(4)').text().split('\n').pop().slice(13);
+            } else {
+                menu.mainDish = dishes.children('p:nth-child(5)').text().slice(13)
+            }
+
             console.log(menu)
         })
         return menu
