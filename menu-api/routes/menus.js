d4e97c3afb2953b26d50b3324c5afc4e9787edd6
Peter Nagy <peter.nagy@adevinta.com>
2020-11-06 12:09:59 +0100

    Add basic cahce for menu endpoints

--- menu-api/routes/menus.js
+++ menu-api/routes/menus.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const router = express.Router();
+
 const MenuCrawler = require('../public/javascripts/MenuCrawler');
-let router = express.Router();
-let crawler = new MenuCrawler();
+const crawler = new MenuCrawler();
+
+const ExpressCache = require('express-cache-middleware')
+const cacheManager = require('cache-manager')
+
+const cacheMiddleware = new ExpressCache(
+  cacheManager.caching({
+      store: 'memory', max: 10000, ttl: 3600
+  })
+)
+cacheMiddleware.attach(router)
 
 router.get('/menus/:menu', async function(req, res, next) {
   res.json(await crawler.getMenu(req.params.menu))
