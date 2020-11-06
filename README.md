97655903126709aba00a6b0cba4862a92dc68d79
Peter Nagy <peter.nagy@adevinta.com>
2020-11-06 16:42:37 +0100

    Create basic react page using scematnic ui
    Create MenuAdapter to call the api

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # node-crawler-react-web
+
+### Run the app on your machine:
+
+Requirement: `npm`
+
+1. Fire up backend:
+```bash
+cd menu-api
+
+npm i
+
+npm start
+```
+2. Run web client:
+```bash
+cd menu-client
+
+npm i
+
+npm start
+```
+ - The page will be available on https://localhost:3000
+
---
+++ menu-client/src/components/MenuAdapter.js
@@ -0,0 +1,44 @@
+import { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+
+class MenuAdapter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { menu: {} }
+    }
+
+    async fetchData() {
+        fetch(`http://localhost:8080/api/menus/${this.props.option}`)
+            .then(res => res.json())
+            .then(res => { this.setState({ menu: this.transformMenu(res) }) })
+            .catch(err => err)
+    }
+
+    async componentDidMount() {
+        await this.fetchData()
+    }
+
+    transformMenu = (menu) => {
+        return {
+            title: menu.title.toUpperCase(),
+            date: new Date().toDateString(),
+            price: menu.price,
+            soup: menu.soup,
+            mainDish: menu.mainDish,
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+                <h2>Menu {this.state.menu.title}</h2>
+                <h3>{this.state.menu.date}</h3>
+                <p>Price : {this.state.menu.price}</p>
+                <p>Soup : {this.state.menu.soup}</p>
+                <p>Main dish : {this.state.menu.mainDish}</p>
+            </Container>
+        )
+    }
+}
+
+export default MenuAdapter
